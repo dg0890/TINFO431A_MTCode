d15b1c52098792832de91b2834c6c8d4ba527716
David Guerrero <dg0890@uw.edu>
2018-03-03 22:52:13 -0800

    commit for MTCode

---
+++ ContosoUniversity/Models/Student.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ContosoUniversity.Models
+{
+    public class Student : Person
+    {
+        //[DisplayFormat(NullDisplayText = "No Credits Earned")]
+        public int CreditsEarned { get; set; }
+
+        //[DisplayFormat(NullDisplayText = "No GPA")]
+        [Range(0.7, 4.0)]
+        public double GPA { get; set; }
+
+        
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Enrollment Date")]
+        public DateTime EnrollmentDate { get; set; }
+
+        public virtual ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
